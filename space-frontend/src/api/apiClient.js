12343602edaf42710511aade190a2124d339c50a
Spike172(VS) <tejshaldankar@gmail.com>
2025-11-12 20:40:23 -0800

    WHY ISNT THIS WORKING???

--- space-frontend/src/api/apiClient.js
+++ space-frontend/src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://space-management-api.onrender.com/', // LOOK HERE 0_0
+  baseURL: 'https://space-management-api.onrender.com/upload', // LOOK HERE 0_0
   timeout: 30000,
   headers: { 'Content-Type': 'application/json' },
 })
