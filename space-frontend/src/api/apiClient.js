5d3487489653422cf03d9a70b3bcf2f8b20c4588
Spike172(VS) <tejshaldankar@gmail.com>
2025-11-12 20:19:33 -0800

    longer timeout due to render sleep

--- space-frontend/src/api/apiClient.js
+++ space-frontend/src/api/apiClient.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
   baseURL: 'https://space-management-api.onrender.com/', // LOOK HERE 0_0
-  timeout: 15000,
+  timeout: 30000,
   headers: { 'Content-Type': 'application/json' },
 })
 
