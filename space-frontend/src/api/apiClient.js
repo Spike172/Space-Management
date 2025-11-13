2e45c5eccf26b8e549cb957c722caa3dbf3aa692
Spike172(VS) <tejshaldankar@gmail.com>
2025-11-12 20:45:36 -0800

    PLEEEEASE WOOOORK

--- space-frontend/src/api/apiClient.js
+++ space-frontend/src/api/apiClient.js
@@ -3,7 +3,6 @@ import axios from 'axios'
 const api = axios.create({
   baseURL: 'https://space-management-api.onrender.com/upload', // LOOK HERE 0_0
   timeout: 30000,
-  headers: { 'Content-Type': 'application/json' },
 })
 
 export default api
